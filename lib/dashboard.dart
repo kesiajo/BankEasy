93380589036addfc4fc82f4195549724a92ebf4f
NehaJijy <nehajijy@gmail.com>
2020-12-27 19:18:14 +0400

    Dashboard added

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.black);
+    return GridView(
+      children: <Widget>[
+        Card(
+          elevation: 10,
+          child: Center(child: Text('Cash Deposit')),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          margin: EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 4,
+          ),
+        ),
+        Card(
+          elevation: 10,
+          child: Center(child: Text('Cash Withdrawal')),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          margin: EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 4,
+          ),
+        ),
+      ],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
   }
 }
